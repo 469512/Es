fff5598c73aff14a61ce31545577212095f41e9c
Isaias F. dos Santos <isaias.fran@gmail.com>
2018-02-15 18:16:02 -0200

    mensagens

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -7,4 +7,76 @@
 		$('#desk-nav').slideToggle(200);
 	});
 
+	// handle menu
+	function menuHeight (){
+		const screenH = window.innerHeight;
+		const screenW = window.innerWidth;
+		const menuH = $('#topo').height();
+		const banners = $('#banner-principal');
+
+		if(screenW < 986)
+		{
+			// adiciona margin top para os banners
+			$( banners ).css('margin-top', menuH );
+			$('#topo').addClass('redu');
+		}
+		return;
+	}
+	$('#banners').slick({
+		arrows: true,
+		infinite: true,
+		speed: 500,
+		fade: true,
+		cssEase: 'linear',
+		pauseOnFocus: false,
+		pauseOnHover: false,
+		nextArrow: '<img src="img/seta-right.png" class="seta-right" />"',
+		prevArrow: '<img src="img/seta-left.png" class="seta-left" />"'
+	  });
+
+	  $('#cursos-todos').slick({
+		dots: false,
+		infinite: false,//Element
+		appendArrows: $(".arrowsPlace"),
+		speed: 300,
+		slidesToShow: 4,
+		slidesToScroll: 4,
+		nextArrow: '<img src="img/seta-cat-right.png" class="seta-right" />"',
+		prevArrow: '<img src="img/seta-cat-left.png" class="seta-left" />"',
+		responsive: [
+		  {
+			breakpoint: 1024,
+			settings: {
+			  slidesToShow: 3,
+			  slidesToScroll: 3,
+			  infinite: true,
+			  dots: true
+			}
+		  },
+		  {
+			breakpoint: 600,
+			settings: {
+			  slidesToShow: 2,
+			  slidesToScroll: 2
+			}
+		  },
+		  {
+			breakpoint: 480,
+			settings: {
+			  slidesToShow: 1,
+			  slidesToScroll: 1
+			}
+		  }
+		  // You can unslick at a given breakpoint now by adding:
+		  // settings: "unslick"
+		  // instead of a settings object
+		]
+	  });
+
+	  window.addEventListener('load', function(){
+		menuHeight()
+	  });
+	  window.addEventListener('resize', function(){
+		menuHeight()
+	  });
 })($)
